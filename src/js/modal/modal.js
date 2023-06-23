7d234bc7c8ffb1ab7f027de67a2d80f7bcb8e85e
Ian <33319@ma-web.nl>
2023-06-23 14:15:56 +0200

    add layout model project

---
+++ src/js/modal/modal.js
@@ -0,0 +1,38 @@
+const project_One = document.getElementById("js--project-1");
+const projects = document.getElementsByName("projectCard__seeMore");
+
+const header = document.getElementById("js--header");
+const projectsPage = document.getElementById("js--projectsPage");
+const footer = document.getElementById("js--footer");
+
+const projectPage = document.getElementById("js--projectsCard");
+
+const exitButton = document.getElementById("js--closeButton");
+const returnButton = document.getElementById("js--returnButton");
+
+project_One.onclick = function(){
+    // for(let i = 0; i < projects.length; i++){
+    //     projects[i].style.display = "none";
+    // }
+    projectsPage.style.display = "none";
+    header.style.display = "none";
+    footer.style.display = "none";
+
+    projectPage.style.display = "flex";
+}, 
+
+exitButton.onclick = function(){
+    projectsPage.style.display = "flex";
+    header.style.display = "flex";
+    footer.style.display = "flex";
+
+    projectPage.style.display = "none";
+};
+
+returnButton.onclick = function(){
+    projectsPage.style.display = "flex";
+    header.style.display = "flex";
+    footer.style.display = "flex";
+
+    projectPage.style.display = "none";
+};
